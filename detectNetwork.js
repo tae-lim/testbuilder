f038c670c11efa0fe23d1b1d17f1bb3047bb743e
tae-lim <taelim92@gmail.com>
2018-06-20 14:14:37 -0700

    Syntax changes to include substr and length use

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,15 +1,19 @@
 var detectNetwork = function(cardNumber) {
-  if (cardNumber.length === 14 && cardNumber[0] === '3' && (cardNumber[1] === '8' || cardNumber[1] === '9')) {
+  var length = cardNumber.length;
+  var subStr1 = cardNumber.substr(0, 1);
+  var subStr2 = cardNumber.substr(0, 2);
+  if (length === 14 && (subStr2 === '38' || subStr2 === '39')) {
   	return 'Diner\'s Club';
   }
-  if (cardNumber.length === 15 && cardNumber[0] === '3' && (cardNumber[1] === '4' || cardNumber[1] === '7')) {
+  if (length === 15 && (subStr2 === '34' || subStr2 === '37')) {
   	return 'American Express';
   }
-  if ((cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19) && cardNumber[0] === '4') {
+  if ((length === 13 || length === 16 || length === 19) && subStr1 === '4') {
   	return 'Visa';
   }
-  if (cardNumber.length === 16 && cardNumber[0] === '5' && (cardNumber[1] === '1' || cardNumber[1] === '2' || cardNumber[1] === '3' || cardNumber[1] === '4' || cardNumber[1] === '5')) {
+  if (length === 16 && (subStr2 === '51' || subStr2 === '52' || subStr2 === '53' || subStr2 === '54' || subStr2 === '55')) {
   	return 'MasterCard';
   }
+  if ((length === 16 || length === 19) && cardNumber[0] === '6' && cardNumber[1] === '0' && cardNumber[2] === '1' && cardNumber[3] === '1')
+  	return 'Discover';
 };
-
